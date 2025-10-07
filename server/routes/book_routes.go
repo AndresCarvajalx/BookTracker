0c1aade2203da7c6e207035b41b49f4274e724c0
AndresCarvajalx <andresfelipecarvajalduran3@gmail.com>
2025-10-06 22:47:52 -0500

    Added new endpoints

--- server/routes/book_routes.go
+++ server/routes/book_routes.go
@@ -13,6 +13,8 @@ func BookRoutes(r *gin.Engine) {
 		books.POST("/", controller.AddBook)
 		books.DELETE("/:book_id", controller.DeleteBook)
 		books.PUT("/:book_id", controller.UpdateBook)
-		books.GET("/pdf/:book_id", controller.GetBookFile)
+		books.GET("/:book_id/pdf", controller.GetBookFile)
+		books.PUT("/:book_id/pdf", controller.UpdateBookFile)
+		books.PUT("/:book_id/cover", controller.UpdateBookCover)
 	}
 }
